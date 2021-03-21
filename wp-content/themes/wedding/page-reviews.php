444af13d89dd609761005a9f42fee4345502f235
Dmitr Drobotov <drobotoff@gmail.com>
2021-03-21 23:35:22 +0600

    Reviews page

---
+++ wp-content/themes/wedding/page-reviews.php
@@ -0,0 +1,257 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Wedding 1.0
+ */
+
+get_header();
+?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+
+            <div class="bread-crumbs-container">
+
+                <div class="container">
+
+                    <ul>
+                        <li>
+                            <a href="/">Главная</a>
+                        </li>
+                        <li>
+                            Отзывы
+                        </li>
+                    </ul>
+                    <div style="clear: both;"></div>
+
+                </div>
+
+            </div>
+
+            <header class="entry-header">
+                <div class="container">
+                    <h1 class="entry-title">Отзывы</h1>
+                </div>
+            </header>
+
+            <div class="review-description-container">
+                <div class="container">
+                    <div class="grid">
+                        <div class="grid__col-8">
+                            <div class="review-description">
+                                <p>Сегодня в нашей Академии обучилось уже более 300 человек по трем направлениям: начинающие мастера, любители шитья, продвинутые мастера</p>
+                            </div>
+
+                            <div class="items">
+
+                                <div class="item">
+
+                                    <div class="info-name">
+                                        <div class="info-name-image">
+                                            <img class="img" src="/wp-content/themes/wedding/images/review-image.png" alt="">
+                                        </div>
+                                        <div class="info-name-text">
+                                            <div class="name"><strong>Юлия блаблабла</strong> | 35 лет, Москва</div>
+                                            <div class="cat-name">Мастер высшей категории</div>
+                                        </div>
+                                        <div style="clear: both;"></div>
+                                    </div>
+
+                                    <div class="info-text">
+                                        О Юленьке я узнала на форуме Академии корсета,сначала просто часами рассматривала её работы.Все кто видел со мной согласятся,что это просто восторг!!!!!Поэтому когда вышел первый Юлин диск я без раздумий его заказала,знания и опыт которыми делится Юля просто бесценны!Уроки изложены грамотно,доступно и понятно!!С тех пор я приобретаю все Юлины диски
+                                    </div>
+
+                                    <div class="info-text-more">
+                                        Показать весь отзыв
+                                    </div>
+
+                                </div>
+
+                                <div class="item">
+
+                                    <div class="info-name">
+                                        <div class="info-name-image">
+                                            <img class="img" src="/wp-content/themes/wedding/images/review-image.png" alt="">
+                                        </div>
+                                        <div class="info-name-text">
+                                            <div class="name"><strong>Юлия блаблабла</strong> | 35 лет, Москва</div>
+                                            <div class="cat-name">Мастер высшей категории</div>
+                                        </div>
+                                        <div style="clear: both;"></div>
+                                    </div>
+
+                                    <div class="info-text">
+                                        О Юленьке я узнала на форуме Академии корсета,сначала просто часами рассматривала её работы.Все кто видел со мной согласятся,что это просто восторг!!!!!Поэтому когда вышел первый Юлин диск я без раздумий его заказала,знания и опыт которыми делится Юля просто бесценны!Уроки изложены грамотно,доступно и понятно!!С тех пор я приобретаю все Юлины диски
+                                    </div>
+
+                                    <div class="info-text-more">
+                                        Показать весь отзыв
+                                    </div>
+
+                                </div>
+
+                                <div class="item">
+
+                                    <div class="info-name">
+                                        <div class="info-name-image">
+                                            <img class="img" src="/wp-content/themes/wedding/images/review-image.png" alt="">
+                                        </div>
+                                        <div class="info-name-text">
+                                            <div class="name"><strong>Юлия блаблабла</strong> | 35 лет, Москва</div>
+                                            <div class="cat-name">Мастер высшей категории</div>
+                                        </div>
+                                        <div style="clear: both;"></div>
+                                    </div>
+
+                                    <div class="info-text">
+                                        О Юленьке я узнала на форуме Академии корсета,сначала просто часами рассматривала её работы.Все кто видел со мной согласятся,что это просто восторг!!!!!Поэтому когда вышел первый Юлин диск я без раздумий его заказала,знания и опыт которыми делится Юля просто бесценны!Уроки изложены грамотно,доступно и понятно!!С тех пор я приобретаю все Юлины диски
+                                    </div>
+
+                                    <div class="info-text-more">
+                                        Показать весь отзыв
+                                    </div>
+
+                                </div>
+
+                                <div class="item">
+
+                                    <div class="info-name">
+                                        <div class="info-name-image">
+                                            <img class="img" src="/wp-content/themes/wedding/images/review-image.png" alt="">
+                                        </div>
+                                        <div class="info-name-text">
+                                            <div class="name"><strong>Юлия блаблабла</strong> | 35 лет, Москва</div>
+                                            <div class="cat-name">Мастер высшей категории</div>
+                                        </div>
+                                        <div style="clear: both;"></div>
+                                    </div>
+
+                                    <div class="info-text">
+                                        О Юленьке я узнала на форуме Академии корсета,сначала просто часами рассматривала её работы.Все кто видел со мной согласятся,что это просто восторг!!!!!Поэтому когда вышел первый Юлин диск я без раздумий его заказала,знания и опыт которыми делится Юля просто бесценны!Уроки изложены грамотно,доступно и понятно!!С тех пор я приобретаю все Юлины диски
+                                    </div>
+
+                                    <div class="info-text-more">
+                                        Показать весь отзыв
+                                    </div>
+
+                                </div>
+
+                                <div class="item">
+
+                                    <div class="info-name">
+                                        <div class="info-name-image">
+                                            <img class="img" src="/wp-content/themes/wedding/images/review-image.png" alt="">
+                                        </div>
+                                        <div class="info-name-text">
+                                            <div class="name"><strong>Юлия блаблабла</strong> | 35 лет, Москва</div>
+                                            <div class="cat-name">Мастер высшей категории</div>
+                                        </div>
+                                        <div style="clear: both;"></div>
+                                    </div>
+
+                                    <div class="info-text">
+                                        О Юленьке я узнала на форуме Академии корсета,сначала просто часами рассматривала её работы.Все кто видел со мной согласятся,что это просто восторг!!!!!Поэтому когда вышел первый Юлин диск я без раздумий его заказала,знания и опыт которыми делится Юля просто бесценны!Уроки изложены грамотно,доступно и понятно!!С тех пор я приобретаю все Юлины диски
+                                    </div>
+
+                                    <div class="info-text-more">
+                                        Показать весь отзыв
+                                    </div>
+
+                                </div>
+
+                                <div class="item">
+
+                                    <div class="info-name">
+                                        <div class="info-name-image">
+                                            <img class="img" src="/wp-content/themes/wedding/images/review-image.png" alt="">
+                                        </div>
+                                        <div class="info-name-text">
+                                            <div class="name"><strong>Юлия блаблабла</strong> | 35 лет, Москва</div>
+                                            <div class="cat-name">Мастер высшей категории</div>
+                                        </div>
+                                        <div style="clear: both;"></div>
+                                    </div>
+
+                                    <div class="info-text">
+                                        О Юленьке я узнала на форуме Академии корсета,сначала просто часами рассматривала её работы.Все кто видел со мной согласятся,что это просто восторг!!!!!Поэтому когда вышел первый Юлин диск я без раздумий его заказала,знания и опыт которыми делится Юля просто бесценны!Уроки изложены грамотно,доступно и понятно!!С тех пор я приобретаю все Юлины диски
+                                    </div>
+
+                                    <div class="info-text-more">
+                                        Показать весь отзыв
+                                    </div>
+
+                                </div>
+
+                                <div class="item">
+
+                                    <div class="info-name">
+                                        <div class="info-name-image">
+                                            <img class="img" src="/wp-content/themes/wedding/images/review-image.png" alt="">
+                                        </div>
+                                        <div class="info-name-text">
+                                            <div class="name"><strong>Юлия блаблабла</strong> | 35 лет, Москва</div>
+                                            <div class="cat-name">Мастер высшей категории</div>
+                                        </div>
+                                        <div style="clear: both;"></div>
+                                    </div>
+
+                                    <div class="info-text">
+                                        О Юленьке я узнала на форуме Академии корсета,сначала просто часами рассматривала её работы.Все кто видел со мной согласятся,что это просто восторг!!!!!Поэтому когда вышел первый Юлин диск я без раздумий его заказала,знания и опыт которыми делится Юля просто бесценны!Уроки изложены грамотно,доступно и понятно!!С тех пор я приобретаю все Юлины диски
+                                    </div>
+
+                                    <div class="info-text-more">
+                                        Показать весь отзыв
+                                    </div>
+
+                                </div>
+
+                                <div class="item">
+
+                                    <div class="info-name">
+                                        <div class="info-name-image">
+                                            <img class="img" src="/wp-content/themes/wedding/images/review-image.png" alt="">
+                                        </div>
+                                        <div class="info-name-text">
+                                            <div class="name"><strong>Юлия блаблабла</strong> | 35 лет, Москва</div>
+                                            <div class="cat-name">Мастер высшей категории</div>
+                                        </div>
+                                        <div style="clear: both;"></div>
+                                    </div>
+
+                                    <div class="info-text">
+                                        О Юленьке я узнала на форуме Академии корсета,сначала просто часами рассматривала её работы.Все кто видел со мной согласятся,что это просто восторг!!!!!Поэтому когда вышел первый Юлин диск я без раздумий его заказала,знания и опыт которыми делится Юля просто бесценны!Уроки изложены грамотно,доступно и понятно!!С тех пор я приобретаю все Юлины диски
+                                    </div>
+
+                                    <div class="info-text-more">
+                                        Показать весь отзыв
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="grid__col-4">
+
+
+                        </div>
+                    </div>
+
+                    <div>
+                        <button class="wd-button-white wd-100">Показать больше</button>
+                    </div>
+                </div>
+            </div>
+
+
+    </div>
+
+    </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php
+get_footer();
